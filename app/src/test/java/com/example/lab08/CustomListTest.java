4948f930c98f3428603c9f160011a13aaa39a859
koyinsola18 <koyinsolatitiloye@gmail.com>
2026-03-06 15:42:42 -0700

    test CI 3

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,13 +1,12 @@
 package com.example.lab08;
-
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomListTest {
 
     @Test
     public void testHasCity() {
-        CustomList list = new CustomList(null, null);
+        CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
         assertTrue(list.hasCity(calgary));
